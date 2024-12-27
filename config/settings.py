807ae78bb63246a6097a9dba89c3aa393feb4baa
javohir2003s <abdualiyevjavohirn1@gmail.com>
2024-12-27 22:43:36 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,14 @@ DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["134.122.66.117", "127.0.0.1"]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "*",
+    "134.122.66.117",
+    # Qo'shimcha ruxsat berilgan domenlarni bu yerda kiritishingiz mumkin
+]
+
 
 # Application definition
 
@@ -41,10 +49,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-
+    'corsheaders',
 ]
 
+
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
