6922f2d3ea77788bb614993380cd71057ecca0c8
javohir2003s <abdualiyevjavohirn1@gmail.com>
2024-12-27 21:30:42 +0500

    added staticfiles

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["134.122.66.117"]
+ALLOWED_HOSTS = ["134.122.66.117", "127.0.0.1"]
 
 
 # Application definition
